90aeb71e47ee05d2dac91d1d87065b8901faf54d
Amazingwujun <85998282@qq.com>
2023-11-07 17:46:07 +0800

    feat: 测试页面加入百度地图

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,11 +1,22 @@
-import {Card} from "antd";
+import {Card, Flex} from "antd";
+import {useEffect} from "react";
 
-function TestPage(){
+function TestPage() {
+
+    useEffect(() => {
+        const BMapGL = window.BMapGL
+        const map = new BMapGL.Map("container");
+        const point = new BMapGL.Point(116.404, 39.915);
+        map.centerAndZoom(point, 15);
+        map.enableScrollWheelZoom(true); //鼠标缩放
+        var zoomCtrl = new BMapGL.ZoomControl();  // (地图右下角+ - 缩放按钮) 添加缩放控件
+        map.addControl(zoomCtrl);
+    }, [])
+
     return (
         <Card className='full-container' title='测试页面'>
-            <div>
-                Only for test
-            </div>
+                <div id="container" className='full-container'>
+                </div>
         </Card>
     )
 }
