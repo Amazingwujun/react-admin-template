f631bda6a5c48a83787943f5a7987a9aa932314a
Amazingwujun <85998282@qq.com>
2023-11-15 17:20:50 +0800

    fix: 修复 useState() 丢失报文的问题，改用 useReducer

--- src/pages/user-admin/DebugLogPage.jsx
+++ src/pages/user-admin/DebugLogPage.jsx
@@ -1,20 +1,27 @@
 import {Badge, Button, Input, message, Select, Space} from "antd";
 import {useWebSocket} from "ahooks";
-import {useLayoutEffect, useRef, useState} from "react";
+import {useLayoutEffect, useReducer, useRef, useState} from "react";
 import CardX from "../../components/CardX.jsx";
 import repository from "../../utils/repository.js";
 import {USER_INFO_KEY} from "../../const/common.js";
+
+const ActionType = {
+    CLEAR: 0,
+    NEW_MESSAGE: 1
+}
 
 const ConsoleStyle = {
     resize: 'none',
     height: '100%',
     width: '100%',
-    fontFamily: 'consolas',
-    color: '#F2F2F2',
-    background: "#0C0C0C",
-    borderRadius: 5,
-    fontSize: 15,
+    fontFamily: 'Cascadia Mono',
+    color: '#D3D7CF',
+    background: "#000000",
+    borderRadius: 2,
+    fontSize: 14,
+    fontWeight: 'normal',
     padding: '0px 5px',
+    outline: 'none',
 }
 
 const ReadyState = {
@@ -37,6 +44,13 @@ function statusColor(readyState) {
     }
 }
 
+function msgReduce(state, action) {
+    if (action.type === ActionType.CLEAR) {
+        return "";
+    }
+    return state + action.data;
+}
+
 
 function DebugLogPage() {
     const ref = useRef();
@@ -55,7 +69,7 @@ function DebugLogPage() {
         }
     });
 
-    const [messages, setMessages] = useState('');
+    const [messages, dispatch] = useReducer(msgReduce, "");
     const {readyState, disconnect, connect} = useWebSocket(
         `${prefix}${url}?x-token=${token}`, {
             manual: true,
@@ -63,12 +77,10 @@ function DebugLogPage() {
                 message.success("连接成功")
             },
             onMessage: message => {
-                setMessages(messages.concat(message.data))
+                dispatch({data: message.data})
             },
             onClose: () => {
                 message.warning("连接已断开")
-                setMessages(messages.concat(`连接已断开 \r\n`));
-                ref.current.scrollTop = ref.current.scrollHeight;
             }
         }
     );
@@ -99,7 +111,7 @@ function DebugLogPage() {
                        style={{width: 400}}/>
                 <Button type='primary' onClick={connect}>发起连接</Button>
                 <Button type='primary' onClick={disconnect}>断开连接</Button>
-                <Button type='primary' onClick={() => setMessages('')}>清空日志</Button>
+                <Button type='primary' onClick={() => dispatch({type: ActionType.CLEAR})}>清空日志</Button>
             </Space>
         )
     }
